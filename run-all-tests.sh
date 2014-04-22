ce08d956dc9f01d26ab363fd20557b0377b6c288
Daniel King <daniel.zidan.king@gmail.com>
2014-04-22 17:36:54 -0400

    Turn off shill_debug when running tests

--- run-all-tests.sh
+++ run-all-tests.sh
@@ -9,8 +9,16 @@ PATH_TO_TEST_LOGS=~/tests/results
 PATH_TO_SHILL=~/shill
 PATH_TO_TESTS=~/tests
 
+echo ">> I'm going to call su now, be ready with root's password!"
+su root -c 'sysctl security.mac.shill.shill_debug=0'
+
+echo ">> NB: I'm not running find-no-kmod-script.sh!"
+echo
+
 for s in ${SCRIPTS[*]}
 do
     echo ">> Executing $s"
     bash $s $PATH_TO_LINUX $PATH_TO_TEST_LOGS $PATH_TO_SHILL 1 $PATH_TO_TESTS
 done
+
+su root -c 'sysctl security.mac.shill.shill_debug=1'
