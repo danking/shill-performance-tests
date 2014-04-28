ba275ae07f5f45b22853c8dbd8bf03bed37c14a5
Daniel King <daniel.zidan.king@gmail.com>
2014-04-28 19:22:05 -0400

    Add c file which prints current nanoseconds

---
+++ nanoseconds.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/time.h>
+int main(void)
+{
+  struct timeval time_now;
+    gettimeofday(&time_now,NULL);
+    printf ("before_everything %ld usecs\n",time_now.tv_sec * 100000 + time_now.tv_usec);
+
+    return 0;
+}
+
