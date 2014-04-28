2b2cfacffc5ddd65ce892562819a83fcae3f2ea3
Daniel King <daniel.zidan.king@gmail.com>
2014-04-28 18:50:04 -0400

    working

---
+++ individual-system-call-tests/open-read-write.c
@@ -0,0 +1,59 @@
+#include <errno.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+  struct timeval start, end;
+  long fopen_time, fread_time, fwrite_time, secs, usecs;
+
+  gettimeofday(&start, NULL);
+  FILE * f = fopen("/home/danking/tests/individual-system-call-tests/test-file", "w");
+  gettimeofday(&end, NULL);
+  secs  = end.tv_sec  - start.tv_sec;
+  usecs = end.tv_usec - start.tv_usec;
+  fopen_time = (secs * 1000000 + usecs);
+  if (f == NULL) {
+    perror("fopen");
+    return errno;
+  }
+
+  char s[11] = "hello world";
+  int length = 11;
+  int err = 0;
+
+  gettimeofday(&start, NULL);
+  err = fwrite(&s, sizeof(char), length, f);
+  gettimeofday(&end, NULL);
+  secs  = end.tv_sec  - start.tv_sec;
+  usecs = end.tv_usec - start.tv_usec;
+  fwrite_time = (secs * 1000000 + usecs);
+  if (err != length) {
+    perror("fwrite");
+    return errno;
+  }
+
+  fclose(f);
+  f = fopen("/home/danking/tests/individual-system-call-tests/test-file", "r");
+
+  char buf[length];
+
+  gettimeofday(&start, NULL);
+  err = fread(&buf, sizeof(char), length, f);
+  gettimeofday(&end, NULL);
+  secs  = end.tv_sec  - start.tv_sec;
+  usecs = end.tv_usec - start.tv_usec;
+  fread_time = (secs * 1000000 + usecs);
+  if (err != length) {
+    perror("fread");
+    return errno;
+  }
+
+  fclose(f);
+
+  printf("fopen: %ld\n", fopen_time);
+  printf("fread: %ld\n", fread_time);
+  printf("fwrite: %ld\n", fwrite_time);
+
+  return 0;
+}
