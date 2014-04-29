b314c1b9e2103e69b054c83da524d0aaab8f7a04
Daniel King <daniel.zidan.king@gmail.com>
2014-04-28 20:31:00 -0400

    fix unit conversion

--- nanoseconds.c
+++ nanoseconds.c
@@ -4,7 +4,7 @@ int main(void)
 {
   struct timeval time_now;
     gettimeofday(&time_now,NULL);
-    printf ("before_everything %ld usecs\n",time_now.tv_sec * 100000 + time_now.tv_usec);
+    printf ("before_everything %ld usecs\n",time_now.tv_sec * 1000000 + time_now.tv_usec);
 
     return 0;
 }
