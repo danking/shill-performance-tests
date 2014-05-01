c40549e726ba35f0506007b708b227957ad5dc71
Dan King <daniel.zidan.king@gmail.com>
2014-05-01 02:33:21 -0400

    Switch to using open, read, write instead of fread, ...

--- individual-system-call-tests/open-read-write.c
+++ individual-system-call-tests/open-read-write.c
@@ -1,5 +1,7 @@
+#include <fcntl.h>
 #include <errno.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/time.h>
 #include <unistd.h>
 
@@ -7,16 +9,16 @@
 
 int main(int argc, char** argv) {
   struct timeval start, end;
-  long fopen_time, fread_time, fwrite_time, secs, usecs;
+  long open_time, read_time, write_time, secs, usecs;
 
   gettimeofday(&start, NULL);
-  FILE * f = fopen(CONTAINING_DIRECTORY "test-file", "w");
+  int f = open(CONTAINING_DIRECTORY "test-file", O_WRONLY);
   gettimeofday(&end, NULL);
   secs  = end.tv_sec  - start.tv_sec;
   usecs = end.tv_usec - start.tv_usec;
-  fopen_time = (secs * 1000000 + usecs);
-  if (f == NULL) {
-    perror("fopen");
+  open_time = (secs * 1000000 + usecs);
+  if (f == -1) {
+    perror("open");
     return errno;
   }
 
@@ -25,35 +27,39 @@ int main(int argc, char** argv) {
   int err = 0;
 
   gettimeofday(&start, NULL);
-  err = fwrite(&s, sizeof(char), length, f);
+  err = write(f, &s, sizeof(char) * length);
   gettimeofday(&end, NULL);
   secs  = end.tv_sec  - start.tv_sec;
   usecs = end.tv_usec - start.tv_usec;
-  fwrite_time = (secs * 1000000 + usecs);
+  write_time = (secs * 1000000 + usecs);
   if (err != length) {
-    perror("fwrite");
+    perror("write");
     return errno;
   }
 
-  fclose(f);
-  f = fopen(CONTAINING_DIRECTORY "test-file", "r");
+  close(f);
+  f = open(CONTAINING_DIRECTORY "test-file", O_RDONLY);
+  if (f == -1) {
+    perror("open");
+    return errno;
+  }
 
   char buf[length];
 
   gettimeofday(&start, NULL);
-  err = fread(&buf, sizeof(char), length, f);
+  err = read(f, &buf, sizeof(char) * length);
   gettimeofday(&end, NULL);
   secs  = end.tv_sec  - start.tv_sec;
   usecs = end.tv_usec - start.tv_usec;
-  fread_time = (secs * 1000000 + usecs);
+  read_time = (secs * 1000000 + usecs);
   if (err != length) {
-    perror("fread");
+    perror("read");
     return errno;
   }
 
-  fclose(f);
+  close(f);
 
-  printf("%ld %ld %ld\n", fopen_time, fread_time, fwrite_time);
+  printf("%ld %ld %ld\n", open_time, read_time, write_time);
 
   return 0;
 }
