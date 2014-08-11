9f595a71eb7f8699ace998fd383f57aa37d18e7c
Daniel King <daniel.zidan.king@gmail.com>
2014-08-11 17:51:49 -0400

    Bump repetitions to 10,000

--- microbenchmarks/w.c
+++ microbenchmarks/w.c
@@ -8,7 +8,7 @@
 #include <errno.h>     /* errno */
 #include <sys/time.h>  /* gettimeofday, struct timeval */
 
-#define REPETITIONS 100
+#define REPETITIONS 10000
 
 int main(int argc, char *argv[]) {
   int err;
