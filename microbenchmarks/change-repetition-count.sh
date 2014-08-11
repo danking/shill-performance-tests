21e994683dd520acb40083a7b5534b2aeb438118
Daniel King <daniel.zidan.king@gmail.com>
2014-08-11 17:42:17 -0400

    Add script to change repetitions

---
+++ microbenchmarks/change-repetition-count.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FILES=(cwu.c mkdir.c orc.c owc.c r.c w.c)
+
+for file in $FILES[@]
+do
+    echo $file
+    TEMPFILE=$(mktemp "$0.XXXXXX")
+    cat $file | sed 's/REPETITIONS ${1}/REPETITIONS ${2}/' > ${TEMPFILE}
+    mv ${TEMPFILE} ${file}
+done
