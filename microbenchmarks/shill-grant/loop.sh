08f67172b6b2497a040390340f6bb323b87729c2
Daniel King <daniel.zidan.king@gmail.com>
2014-08-27 11:19:44 -0400

    Update loop.sh to accept a binary

--- microbenchmarks/shill-grant/loop.sh
+++ microbenchmarks/shill-grant/loop.sh
@@ -1,38 +1,32 @@
 #!/usr/local/bin/bash
 
+function die() { echo "$(basename ${0}): $@" 1>&2 ; exit 1; }
+
 sudo sysctl security.mac.shill.shill_debug=0
 
 RUNS=$1
+EXE=$2
 
-[ "$#" -eq 1 ] || die "Exactly one argument required, $# provided. Valid invocation:
+[ "$#" -eq 2 ] || die "Exactly two arguments required, $# provided. Valid invocation:
 
-  bash generate-global-policy.sh RESULTS_DIR
+  bash generate-global-policy.sh RUNS EXE
 
   - RUNS -- The number of runs of each full suite
+  - EXE -- which executable to run
 "
 
-RESULTS_FILE="shill-grant-results-sandboxed-$(date +%F-%T)"
+RESULTS_DIR=results
+mkdir -p ${RESULTS_DIR}
+
+RESULTS_FILE="${RESULTS_DIR}/${EXE}-results-$(date +%F-%T)"
 touch ${RESULTS_FILE} || die "Could not create ${RESULTS_FILE}"
 for i in $(seq 1 ${RUNS})
 do
-    TARGET_FILE=$(mktemp "$0.test.file.XXXXXX")
+    TARGET_FILE=$(mktemp "$0.test.dir.XXXXXX")
 
-    ./shill-grant "${TARGET_FILE}" >> ${RESULTS_FILE}
+    "${EXE}" "${TARGET_FILE}" >> ${RESULTS_FILE}
 
     rm -rf "${TARGET_FILE}" || die "Could not remove ${TARGET_FILE}"
 done
 
-rm -rf "$0.test.file.*"
-
-RESULTS_FILE="shill-grant-results-unsandboxed-$(date +%F-%T)"
-touch ${RESULTS_FILE} || die "Could not create ${RESULTS_FILE}"
-for i in $(seq 1 ${RUNS})
-do
-    TARGET_FILE=$(mktemp "$0.test.file.XXXXXX")
-
-    ./shill-no-grant "${TARGET_FILE}" >> ${RESULTS_FILE}
-
-    rm -rf "${TARGET_FILE}" || die "Could not remove ${TARGET_FILE}"
-done
-
-rm -rf "$0.test.file.*"
+rm -rf "$0.test.dir.*"
