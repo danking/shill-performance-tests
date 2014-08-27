f971b629a68482ebe15f8153de4c24b32abbfe14
Daniel King <daniel.zidan.king@gmail.com>
2014-08-27 16:41:06 -0400

    update cu and orc to better timers

--- microbenchmarks/cu.c
+++ microbenchmarks/cu.c
@@ -5,51 +5,110 @@
 #include <stdio.h>     /* printf, sscanf */
 #include <stdlib.h>    /* exit */
 #include <errno.h>     /* errno */
-#include <errno.h>     /* errno */
 #include <sys/time.h>  /* gettimeofday, struct timeval */
 
-#define REPETITIONS 1000000
+#include <err.h>
+#include <sys/param.h>
+#include <sys/cpuset.h>
 
 int main(int argc, char *argv[]) {
   int err;
   int fd;
   int bytes;
+  int iters;
   char * path;
   char * buf;
-  struct timeval time_now;
-  int time_start, time_end;
+  struct timespec time_start_spec, time_end_spec;
+  long time_start, time_end;
   int i;
 
-  if (argc != 2) {
-    printf("cu accepts exactly one argument, a path\n");
+  if (argc != 3) {
+    printf("cu accepts exactly two arguments a path and an iteration count\n");
     exit(1);
   }
 
   path = argv[1];
 
-  gettimeofday(&time_now,NULL);
-  time_start = time_now.tv_sec * 1000000 + time_now.tv_usec;
+  err = sscanf(argv[2], "%d", &iters);
+  if (err != 1) {
+    printf("failed to read number of iterations, given %s\n", argv[3]);
+    printf("  real error num %d\n", errno);
+    perror("  sscanf");
+    exit(1);
+  }
 
   /****************************************************************************/
-  /* BEGIN TIMED */
+  /* And There Was One CPU */
 
-  for (i=0; i<REPETITIONS; ++i) {
+  /* Get CPU mask for the current thread */
+  if (cpuset_getaffinity( CPU_LEVEL_WHICH
+                        , CPU_WHICH_TID
+                        , -1
+                        , sizeof(myset)
+                        , &myset
+                        )
+      == -1) {
+    printf("getaffinity failed");
+    exit(1);
+  }
+
+  /* Find first available CPU - don't assume CPU0 is always available */
+  for (i = 0; i < CPU_SETSIZE; i++) {
+    if (CPU_ISSET(i, &myset)) {
+      break;
+    }
+  }
+
+  if (i == CPU_SETSIZE) {
+    printf("Not allowed to run on any CPUs?  How did I print this, then?");
+    exit(1);
+  }
+
+  /* Set new CPU mask */
+  CPU_ZERO(&myset);
+  CPU_SET(i, &myset);
+
+  if (cpuset_setaffinity( CPU_LEVEL_WHICH
+                        , CPU_WHICH_TID
+                        , -1
+                        , sizeof(myset)
+                        , &myset
+                        )
+      == -1) {
+    warn("setaffinity failed");
+  }
+
+  /* And There Was One CPU */
+  /****************************************************************************/
+
+
+  for (i = 1; i <= iters; ++i) {
+    clock_gettime(CLOCK_MONOTONIC_PRECISE, &time_start_spec);
 
     fd = open(path, O_WRONLY | O_CREAT);
+    err = unlink(path);
+
+    clock_gettime(CLOCK_MONOTONIC_PRECISE, &time_end_spec);
+    time_start = time_start_spec.tv_sec * 1000000000 + time_start_spec.tv_nsec;
+    time_end = time_end_spec.tv_sec * 1000000000 + time_end_spec.tv_nsec;
+    printf("%ld\n", time_end - time_start);
+    if (i % (iters / 100) == 0)
+      fprintf(stderr, ".");
+
+
     if (fd == -1) {
       printf("failed to open (create) %s\n", path);
       printf("  real error num %d\n", errno);
       perror("  open");
       exit(1);
     }
-
-    err = unlink(path);
     if (err == -1) {
       printf("failed to unlink the path %s\n", path);
       printf("  real error num %d\n", errno);
       perror("  unlink");
       exit(1);
     }
+
 
     err = close(fd);
     if (err == -1) {
@@ -58,15 +117,15 @@ int main(int argc, char *argv[]) {
       perror("  close");
       exit(1);
     }
+
+    if (err == -1) {
+      printf("failed to read %d bytes\n", bytes);
+      printf("  real error num %d\n", errno);
+      perror("  read");
+      exit(1);
+    }
   }
-
-  /* END TIMED */
-  /****************************************************************************/
-
-  gettimeofday(&time_now,NULL);
-  time_end = time_now.tv_sec * 1000000 + time_now.tv_usec;
-
-  printf("%f\n", (time_end - time_start)/((float)REPETITIONS));
+  fprintf(stderr, " done\n");
 
   return 0;
 }
--- microbenchmarks/orc.c
+++ microbenchmarks/orc.c
@@ -8,20 +8,23 @@
 #include <errno.h>     /* errno */
 #include <sys/time.h>  /* gettimeofday, struct timeval */
 
-#define REPETITIONS 1000000
+#include <err.h>
+#include <sys/param.h>
+#include <sys/cpuset.h>
 
 int main(int argc, char *argv[]) {
-  int err;
+  int err, err2;
   int fd;
   int bytes;
+  int iters;
   char * path;
   char * buf;
-  struct timeval time_now;
-  int time_start, time_end;
+  struct timespec time_start_spec, time_end_spec;
+  long time_start, time_end;
   int i;
 
-  if (argc != 3) {
-    printf("orc accepts exactly two arguments, a byte count and a path\n");
+  if (argc != 4) {
+    printf("orc accepts exactly three arguments, a byte count, a path, and an iteration count\n");
     exit(1);
   }
 
@@ -35,6 +38,14 @@ int main(int argc, char *argv[]) {
 
   path = argv[2];
 
+  err = sscanf(argv[3], "%d", &iters);
+  if (err != 1) {
+    printf("failed to read number of iterations, given %s\n", argv[3]);
+    printf("  real error num %d\n", errno);
+    perror("  sscanf");
+    exit(1);
+  }
+
   buf = malloc(bytes);
   if (buf == NULL) {
     printf("failed to malloc %d bytes\n", bytes);
@@ -43,14 +54,64 @@ int main(int argc, char *argv[]) {
     exit(1);
   }
 
-  gettimeofday(&time_now,NULL);
-  time_start = time_now.tv_sec * 1000000 + time_now.tv_usec;
-
   /****************************************************************************/
-  /* BEGIN TIMED */
+  /* And There Was One CPU */
 
-  for (i=0; i<REPETITIONS; ++i) {
+  /* Get CPU mask for the current thread */
+  if (cpuset_getaffinity( CPU_LEVEL_WHICH
+                        , CPU_WHICH_TID
+                        , -1
+                        , sizeof(myset)
+                        , &myset
+                        )
+      == -1) {
+    printf("getaffinity failed");
+    exit(1);
+  }
+
+  /* Find first available CPU - don't assume CPU0 is always available */
+  for (i = 0; i < CPU_SETSIZE; i++) {
+    if (CPU_ISSET(i, &myset)) {
+      break;
+    }
+  }
+
+  if (i == CPU_SETSIZE) {
+    printf("Not allowed to run on any CPUs?  How did I print this, then?");
+    exit(1);
+  }
+
+  /* Set new CPU mask */
+  CPU_ZERO(&myset);
+  CPU_SET(i, &myset);
+
+  if (cpuset_setaffinity( CPU_LEVEL_WHICH
+                        , CPU_WHICH_TID
+                        , -1
+                        , sizeof(myset)
+                        , &myset
+                        )
+      == -1) {
+    warn("setaffinity failed");
+  }
+
+  /* And There Was One CPU */
+  /****************************************************************************/
+
+  for (i = 1; i < iters; ++i) {
+    clock_gettime(CLOCK_MONOTONIC_PRECISE, &time_start_spec);
+
     fd = open(path, O_RDONLY);
+    err = pread(fd, (void*)buf, bytes, 0);
+    err2 = close(fd);
+
+    clock_gettime(CLOCK_MONOTONIC_PRECISE, &time_end_spec);
+    time_start = time_start_spec.tv_sec * 1000000000 + time_start_spec.tv_nsec;
+    time_end = time_end_spec.tv_sec * 1000000000 + time_end_spec.tv_nsec;
+    printf("%ld\n", time_end - time_start);
+    if (i % (iters / 100) == 0)
+      fprintf(stderr, ".");
+
     if (fd == -1) {
       printf("failed to open %s\n", path);
       printf("  real error num %d\n", errno);
@@ -58,7 +119,6 @@ int main(int argc, char *argv[]) {
       exit(1);
     }
 
-    err = pread(fd, (void*)buf, bytes, 0);
     if (err == -1) {
       printf("failed to read %d bytes\n", bytes);
       printf("  real error num %d\n", errno);
@@ -66,8 +126,7 @@ int main(int argc, char *argv[]) {
       exit(1);
     }
 
-    err = close(fd);
-    if (err == -1) {
+    if (err2 == -1) {
       printf("failed to close the file descriptor\n");
       printf("  real error num %d\n", errno);
       perror("  close");
@@ -77,11 +136,6 @@ int main(int argc, char *argv[]) {
 
   /* END TIMED */
   /****************************************************************************/
-
-  gettimeofday(&time_now,NULL);
-  time_end = time_now.tv_sec * 1000000 + time_now.tv_usec;
-
-  printf("%f\n", (time_end - time_start)/((float)REPETITIONS));
 
   return 0;
 }
